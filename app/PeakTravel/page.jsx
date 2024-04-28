70be0729adc16996349150592dacb09c62fd73cf
“supriyakr” <supriyakr222@gmail.com>
2024-04-27 20:43:25 -0700

    Updated folder structure and imported D3

---
+++ app/PeakTravel/page.jsx
@@ -0,0 +1,12 @@
+
+
+export default async function PeakTravel() {
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+   <h2>Peak Travel Months</h2>
+    
+    </main>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,16 @@
-import {getPeakTravelMonths} from "./api/getData"
+import {getPeakTravelMonths} from "./api/getPeakTravelData"
 
 export default async function Home() {
 
   const peakMonths = await getPeakTravelMonths();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   <h2>SFO Airport Statistics Visualization</h2>
-    <p>Peak Travel Months</p>
-    <ul>
-      {peakMonths.map(month => (
-        <li key={month}>{month}</li>
-      ))}
-      </ul>
+   <h2>SFO Airport Statistics Visualization.
+   
+   </h2>
+    <h3>Landing page</h3>
+   
+   
     </main>
   );
 }
