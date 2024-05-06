34d45962e873a55150cf3e0b174a521884bce533
“supriyakr” <supriyakr222@gmail.com>
2024-05-06 16:12:47 -0700

    fixed some errors

--- app/PeakTravel/PeakMonthsPlot.jsx
+++ app/PeakTravel/PeakMonthsPlot.jsx
@@ -43,7 +43,7 @@ export default function PeakMonthsPlot({ peakMonths }) {
           .attr("transform", "translate(0," + height + ")")
           .call(d3.axisBottom(x));
         // y-axis
-        const y = d3.scaleLinear().domain([0, 7000000]).range([height, 0]);
+        const y = d3.scaleLinear().domain([0, 6000000]).range([height, 0]);
 
         //y-axis formatting
         const yAxis = d3
@@ -58,87 +58,54 @@ export default function PeakMonthsPlot({ peakMonths }) {
 
         svg.append("g").call(yAxis);
 
-        // Add the line
-        const line = d3
-          .line()
-          .x((d, i) => x(d.monthName)) // Using index as x-coordinate
-          .y((d) => y(d.passenger_count));
-
-        svg
+        // Initialize line with group a
+        const line = svg
           .append("path")
           .datum(data[2000])
-          .attr("fill", "none")
+          .attr(
+            "d",
+            d3
+              .line()
+              .x((d) => x(d.monthName))
+              .y((d) => y(d.passenger_count))
+          )
           .attr("stroke", "#007bff")
-          .attr("stroke-width", 2)
-          .attr("d", line);
-        // create a tooltip
+          .style("stroke-width", 2)
+          .style("fill", "none");
 
-        var Tooltip = d3
-          .select("body")
-          .append("div")
-          .style("opacity", 0)
-          .attr("class", "tooltip")
-          .style("background-color", "black")
-          .style("border", "solid")
-          .style("border-width", "2px")
-          .style("border-radius", "5px")
-          .style("padding", "5px");
-
-        // Three function that change the tooltip when user hover / move / leave a cell
-        const mouseover = function (event, d) {
-          Tooltip.style("opacity", 1);
-        };
-        const mousemove = function (event, d) {
-          Tooltip.html("Exact value: " + d.passenger_count)
-            .style("left", `${event.pageX + 10}px`) // Use event.pageX
-            .style("top", `${event.pageY - 28}px`) // Use event.pageY and adjust the offset
-            .style("opacity", 1);
-        };
-        const mouseleave = function (event, d) {
-          Tooltip.style("opacity", 0);
-        };
+        // Initialize dots with group a
+        const dot = svg
+          .selectAll("circle")
+          .data(data[2000])
+          .join("circle")
+          .attr("class", "myCircle")
+          .attr("cx", (d) => x(d.monthName))
+          .attr("cy", (d) => y(d.passenger_count))
+          .attr("r", 7)
+          .style("fill", "#69b3a2");
 
         // A function that update the chart
         function update(selectedGroup) {
           // Create new data with the selection?
           const dataFilter = data[selectedGroup];
-          d3.select("d").remove();
-
           // Give these new data to update line
-          // Add the line
-          const line = d3
-            .line()
-            .x((d, i) => x(d.monthName)) // Using index as x-coordinate
-            .y((d) => y(d.passenger_count));
-          svg
-            .append("path")
+          line
             .datum(dataFilter)
-            .attr("fill", "none")
-            .attr("stroke", "#007bff")
-            .attr("stroke-width", 2)
-            .attr("d", line);
-
-          // Add the points
-
-          svg
-            .append("g")
-            .selectAll("dot")
+            .transition()
+            .duration(1000)
+            .attr(
+              "d",
+              d3
+                .line()
+                .x((d) => x(d.monthName))
+                .y((d) => y(d.passenger_count))
+            );
+          dot
             .data(dataFilter)
-            .join("circle")
-            .attr("class", "myCircle")
-            .attr("cx", (d) => {
-              return x(d.monthName);
-            })
-            .attr("cy", (d) => {
-              return y(d.passenger_count);
-            })
-            .attr("r", 5)
-            .attr("stroke", "#69b3a2")
-            .attr("stroke-width", 3)
-            .attr("fill", "white")
-            .on("mouseover", mouseover)
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave);
+            .transition()
+            .duration(1000)
+            .attr("cx", (d) => x(d.monthName))
+            .attr("cy", (d) => y(d.passenger_count));
         }
 
         // When the button is changed, run the updateChart function
@@ -149,27 +116,6 @@ export default function PeakMonthsPlot({ peakMonths }) {
           console.log("selected option", selectedOption);
           update(selectedOption);
         });
-
-        // Add the points
-        svg
-          .append("g")
-          .selectAll("dot")
-          .data(data[2000])
-          .join("circle")
-          .attr("class", "myCircle")
-          .attr("cx", (d) => {
-            return x(d.monthName);
-          })
-          .attr("cy", (d) => {
-            return y(d.passenger_count);
-          })
-          .attr("r", 5)
-          .attr("stroke", "#69b3a2")
-          .attr("stroke-width", 3)
-          .attr("fill", "white")
-          .on("mouseover", mouseover)
-          .on("mousemove", mousemove)
-          .on("mouseleave", mouseleave);
       }
     };
   }, [peakMonths]);
