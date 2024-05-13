44cfc3f9a27572135d96962a067391435eed5ba2
Shail Patel <patelshail233@gmail.com>
2024-05-13 14:47:24 -0700

    Home screen layout update

--- app/Home/Home.jsx
+++ app/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Tile from './Tile';
 import Image from 'next/image';
 import backgroundImg from '@/public/sf.jpg';
@@ -13,7 +13,7 @@ function Home() {
   ];
 
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center bg-[#FFFFED]">
+    <div className="relative min-h-screen bg-[#FFFFED] flex justify-between">
       <div className="absolute inset-0 z-0">
         <Image 
           src={backgroundImg} 
@@ -24,17 +24,17 @@ function Home() {
           className="opacity-50" 
         />
       </div>
-      <div className="relative z-10 flex flex-col items-center mb-16">
-        <h1 className="text-5xl font-bold mb-4">San Francisco International Airport Data Visualization! ✈️</h1>
+      <div className="z-10 m-auto pl-10">
+        <h1 className="text-5xl font-bold">San Francisco International Airport Data Visualization! ✈️</h1>
       </div>
-      <div className="relative z-10 flex flex-wrap justify-center space-x-4 mb-16">
+      <div className="z-10 m-auto pr-10 flex flex-col items-center space-y-4">
         {tiles.map((tile, index) => (
           <Tile key={index} title={tile.title} path={tile.path} />
         ))}
       </div>
-      <footer className="relative z-10 text-center mt-8">
+      <footer className="absolute bottom-0 left-0 right-0 z-10 text-center p-4">
         <p className="text-sm">
-          Made using real time data provided by <a href="https://data.sfgov.org/Transportation/Air-Traffic-Passenger-Statistics/rkru-6vcg/about_data" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">SF.gov</a>
+          Made using real data provided by <a href="https://data.sfgov.org/Transportation/Air-Traffic-Passenger-Statistics/rkru-6vcg/about_data" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">SF.gov</a>
         </p>
       </footer>
     </div>
--- app/Home/Tile.jsx
+++ app/Home/Tile.jsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 const Tile = ({ title, path }) => {
   return (
     <Link href={path} legacyBehavior>
-      <a className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-4 px-4 rounded-lg transition duration-300 ease-in-out text-sm">
+      <a className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-4 px-4 rounded-lg transition duration-300 ease-in-out text-sm"
+         style={{ width: '250px' }}> {/* Set a specific width */}
         <h3>{title}</h3>
       </a>
     </Link>
